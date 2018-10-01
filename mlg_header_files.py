d2745ec757289431dbed84eafea80530d89f34c8
ARCHANA DASH <adash949@gmail.com>
2018-10-01 10:50:54 +0530

    definition of function called in main code

---
+++ mlg_header_files.py
@@ -0,0 +1,3 @@
+story = ""
+def tell_story(trip):
+    return("Last summer my Mom & Dad took me & {} on a trip to {}. The weather there is very {}! Northern {} has many {}, & they mak    e {} {} there. Many people also go to {} for {} or see the {}. The people that live there love to eat {} & are very proud of their b    ig {}. They also like {} in the sun & swim in the {}! It was really {} trip!".format(trip[0], trip[1], trip[2], trip[3], trip[4], tr    ip[5], trip[6], trip[7], trip[8], trip[9], trip[10], trip[11], trip[12], trip[13], trip[14]))
